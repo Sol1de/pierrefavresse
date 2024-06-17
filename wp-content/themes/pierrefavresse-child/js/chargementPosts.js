d26e99cb60c5c7fe3b8f3eab563dfccb432ad835
Olivier Delmas <olivier.delmas@edu.devinci.fr>
2024-06-17 13:06:57 +0200

    :fire: added animation between posts loading

--- wp-content/themes/pierrefavresse-child/js/chargementPosts.js
+++ wp-content/themes/pierrefavresse-child/js/chargementPosts.js
@@ -1,4 +1,3 @@
-//chargement des posts en arrière plan (evite un chargement de la page complet)
 document.addEventListener('DOMContentLoaded', function() {
     var menuLinks = document.querySelectorAll('.header-nav a');
     
@@ -9,16 +8,30 @@ document.addEventListener('DOMContentLoaded', function() {
             var url = link.getAttribute('href');
             var projectsContainer = document.querySelector('.landing-projects');
             
-            // Charge les nouveaux posts
-            fetch(url)
-                .then(response => response.text())
-                .then(html => {
-                    var temp = document.createElement('div');
-                    temp.innerHTML = html;
-                    var newProjects = temp.querySelector('.landing-projects').innerHTML;
-                    projectsContainer.innerHTML = newProjects;
-                })
-                .catch(error => console.error('Erreur lors du chargement des posts', error));
+            projectsContainer.classList.add('fade-out');
+            
+            // Attendre la fin de l'animation de disparition avant de charger les nouveaux posts
+            setTimeout(function() {
+                // Charger les nouveaux posts
+                fetch(url)
+                    .then(response => response.text())
+                    .then(html => {
+                        var temp = document.createElement('div');
+                        temp.innerHTML = html;
+                        var newProjects = temp.querySelector('.landing-projects').innerHTML;
+                        
+                        // Mettre à jour le contenu avec les nouveaux projets
+                        projectsContainer.innerHTML = newProjects;
+                        projectsContainer.classList.remove('fade-out');
+                        projectsContainer.classList.add('fade-in');
+                    })
+                    .catch(error => console.error('Erreur lors du chargement des posts', error))
+                    .finally(() => {
+                        setTimeout(function() {
+                            projectsContainer.classList.remove('fade-in');
+                        }, 300); // Correspond à la durée de l'animation fadeIn
+                    });
+            }, 300); // Correspond à la durée de l'animation fadeOut
         });
     });
 });
